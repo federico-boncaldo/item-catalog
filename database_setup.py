38849f7b5343d972c3ba96e68939b42f2413b00e
Federico <federico.boncaldo@gmail.com>
2018-07-03 17:14:17 +0100

    Add the data model
    The file defines the objects which represent the structure of the data
    for the catalog.

---
+++ database_setup.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Category(Base):
+	__tablename__ = 'category'
+	
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	description = Column(String(250))
+
+	@property
+	def serialize(self):
+		"""Return object in easily serializeable format"""
+		return {
+			'name': self.name,
+			'description': self.description,
+			'id': self.id
+		}
+
+class Item(Base):
+	__tablename__ = 'item'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	description = Column(String(250))
+	price = Column(String(8))
+	category_id = Column(Integer, ForeignKey('category.id'))
+	category = relationship(Category)
+
+	@property
+	def serialize(self):
+		"""Return object in easily serializeable format"""
+		return {
+			'id': self.id,
+			'name': self.name,
+			'description': self.description,
+			'price': self.price,
+			'category_id': self.category_id
+		}
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)
